4c0f4269aa9d4257730519154c18d9b395d2f94b
Martin KHYARI <martin.khyari@gmail.com>
2025-10-27 16:34:32 +0100

    again wrong name

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className="min-h-screen flex flex-col"> 
-        <Router basename='/2025-Portfolio'>
+        <Router basename='/2025-Porfolio'>
           <NavBar />
           <main className="flex-1">
             <Routes>
