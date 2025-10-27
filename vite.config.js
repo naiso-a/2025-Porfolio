e8ba65ec5636970e1cc1e023a783a13e4ee3d7e5
Martin KHYARI <martin.khyari@gmail.com>
2025-10-27 16:32:22 +0100

    fix [ viteconfig] : wrong name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/2025-Portfolio/',
+  base: '/2025-Porfolio/',
   plugins: [
     react(),
     tailwindcss(),
